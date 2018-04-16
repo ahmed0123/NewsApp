6a2b2ec829459aacdf8f134b170c17e955edf03a
Ahmed Mahmoud Elhendawy <ahm_elhen@yahoo.com>
2018-04-16 11:33:28 +0200

    Add package activity

--- app/src/main/java/com/example/hendawy/newsapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/hendawy/newsapp/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.hendawy.newsapp;
+package com.example.hendawy.newsapp.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.hendawy.newsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
