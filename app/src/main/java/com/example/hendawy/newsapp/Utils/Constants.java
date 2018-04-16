473a8229e657d6005e63768a528e7ec2a3b4bc2d
Ahmed Mahmoud Elhendawy <ahm_elhen@yahoo.com>
2018-04-16 10:58:34 +0200

    Adding API key

--- app/src/main/java/com/example/hendawy/newsapp/Utils/Constants.java
+++ app/src/main/java/com/example/hendawy/newsapp/Utils/Constants.java
@@ -1,5 +1,14 @@
 package com.example.hendawy.newsapp.Utils;
 
+import com.example.hendawy.newsapp.Interface.NewsService;
+import com.example.hendawy.newsapp.Rest.ApiClient;
+
 public class Constants {
     public static final String URL = "https://newsapi.org/v2/";
+
+    public static final String API_KEY = "374a05cab3f24e66b96b06913bd73198";
+
+    public static NewsService getNewsService() {
+        return ApiClient.getClient(URL).create(NewsService.class);
+    }
 }
