0742eafc9848446c2da10ed27cca9fd4c9c14353
Ahmed Mahmoud Elhendawy <ahm_elhen@yahoo.com>
2018-04-16 16:23:38 +0200

    Model class for generating icons from urls using  icon-better-idea.org

---
+++ app/src/main/java/com/example/hendawy/newsapp/model/Icon.java
@@ -0,0 +1,9 @@
+package com.example.hendawy.newsapp.model;
+
+public class Icon {
+    private String url;
+    private int width,height,bytes;
+    private String format,sha1sum;
+    private Object error;
+
+}
---
+++ app/src/main/java/com/example/hendawy/newsapp/model/IconBetterIdea.java
@@ -0,0 +1,8 @@
+package com.example.hendawy.newsapp.model;
+
+import java.util.List;
+
+public class IconBetterIdea {
+    private String url;
+    private List<Icon> icons;
+}
