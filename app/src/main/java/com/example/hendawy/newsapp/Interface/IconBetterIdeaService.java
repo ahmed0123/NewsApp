d47321e8c3f651ac9bcf1f0201996fe1465ac619
Ahmed Mahmoud Elhendawy <ahm_elhen@yahoo.com>
2018-04-16 16:26:32 +0200

    use icon-better-idea.org to generate icons from urls

---
+++ app/src/main/java/com/example/hendawy/newsapp/Interface/IconBetterIdeaService.java
@@ -0,0 +1,12 @@
+package com.example.hendawy.newsapp.Interface;
+
+import com.example.hendawy.newsapp.model.IconBetterIdea;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
+public interface IconBetterIdeaService {
+    @GET
+    Call<IconBetterIdea> getIconUrl(@Url String url);
+}
--- app/src/main/java/com/example/hendawy/newsapp/Utils/Constants.java
+++ app/src/main/java/com/example/hendawy/newsapp/Utils/Constants.java
@@ -1,14 +1,22 @@
 package com.example.hendawy.newsapp.Utils;
 
+import com.example.hendawy.newsapp.Interface.IconBetterIdeaService;
 import com.example.hendawy.newsapp.Interface.NewsService;
-import com.example.hendawy.newsapp.Rest.ApiClient;
+import com.example.hendawy.newsapp.Rest.IconBetterIdeaClient;
+import com.example.hendawy.newsapp.Rest.NewsApiClient;
 
 public class Constants {
-    public static final String URL = "https://newsapi.org/v2/";
+
+    public static final String NEWS_API_URL = "https://newsapi.org/v2/";
+    public static final String ICON_BETTER_IDEA_URL = "https://icons.better-idea.org/";
 
     public static final String API_KEY = "374a05cab3f24e66b96b06913bd73198";
 
     public static NewsService getNewsService() {
-        return ApiClient.getClient(URL).create(NewsService.class);
+        return NewsApiClient.getClient(NEWS_API_URL).create(NewsService.class);
+    }
+
+    public static IconBetterIdeaService getIconService() {
+        return IconBetterIdeaClient.getClient(NEWS_API_URL).create(IconBetterIdeaService.class);
     }
 }
